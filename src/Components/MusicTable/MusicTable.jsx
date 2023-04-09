04ef1ccb2028e7b7ff7c47daa27fefe4d7e67d1f
Chris Pierson <cpierswim@gmail.com>
2023-04-08 23:12:24 -0400

    NEW: Add new form validates before submission

--- src/Components/MusicTable/MusicTable.jsx
+++ src/Components/MusicTable/MusicTable.jsx
@@ -229,6 +229,7 @@ const MusicTable = (props) => {
                   value={newTitle}
                   type="text"
                   onChange={(event) => setNewTitle(event.target.value)}
+                  required
                 ></input>
               </td>
               <td>
@@ -237,6 +238,7 @@ const MusicTable = (props) => {
                   value={newArtist}
                   type="text"
                   onChange={(event) => setNewArtist(event.target.value)}
+                  required
                 ></input>
               </td>
               <td>
@@ -245,6 +247,7 @@ const MusicTable = (props) => {
                   value={newAlbumName}
                   type="text"
                   onChange={(event) => setNewAlbumName(event.target.value)}
+                  required
                 ></input>
               </td>
               <td>
@@ -253,6 +256,7 @@ const MusicTable = (props) => {
                   value={newReleaseDate}
                   type="date"
                   onChange={(event) => setNewReleaseDate(event.target.value)}
+                  required
                 ></input>
               </td>
               <td>
@@ -261,10 +265,23 @@ const MusicTable = (props) => {
                   value={newGenre}
                   type="text"
                   onChange={(event) => setNewGenre(event.target.value)}
+                  required
                 ></input>
               </td>
               <td>
-                <button type="submit">Add</button>
+                <button type="submit" className="btn btn-primary addButton">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-file-earmark-plus"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M8 6.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V11a.5.5 0 0 1-1 0V9.5H6a.5.5 0 0 1 0-1h1.5V7a.5.5 0 0 1 .5-.5z"></path>
+                    <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5L14 4.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h-2z"></path>
+                  </svg>
+                </button>
               </td>
             </tr>
           ) : null}
